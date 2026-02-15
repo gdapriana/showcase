72a9a7cbc703a4ae18cc4223c210b2217ac0a7e8
gdapriana <gedeapriana36@gmail.com>
2026-02-16 00:50:31 +0800

    fix text in hero

--- app/(root)/_components/hero.tsx
+++ app/(root)/_components/hero.tsx
@@ -31,13 +31,13 @@ export default function Hero({ user }: { user?: NotionPageResponse | null }) {
             // />
           )}
           <div className="flex absolute left-0 gap-1 md:top-[20%] top-[5%] flex-col justify-start items-start">
-            <motion.span animate={{ opacity: [0, 1] }} transition={{ duration: 1, ease: ease, delay: 2 }} className="text-primary dark:text-primary mix-blend-screen">
+            <motion.span animate={{ opacity: [0, 1] }} transition={{ duration: 1, ease: ease, delay: 2 }} className="text-primary dark:text-primary md:mix-blend-screen">
               {user?.properties.username.title[0].text.content}
             </motion.span>
             <motion.h1
               animate={{ y: [50, 0], opacity: [0, 1] }}
               transition={{ duration: 2, ease: ease, delay: 1 }}
-              className="text-3xl md:tracking-[-4px] relative uppercase md:text-5xl overflow-hidden font-black mix-blend-screen text-primary dark:text-primary"
+              className="text-3xl md:tracking-[-4px] relative uppercase md:text-5xl overflow-hidden font-black md:mix-blend-screen text-primary dark:text-primary"
             >
               {user?.properties.tags.multi_select[0].name}
             </motion.h1>
@@ -46,11 +46,11 @@ export default function Hero({ user }: { user?: NotionPageResponse | null }) {
             <motion.h1
               animate={{ y: [50, 0], opacity: [0, 1] }}
               transition={{ duration: 2, ease: ease, delay: 1 }}
-              className="text-3xl md:tracking-[-4px] text-end uppercase md:text-5xl font-black mix-blend-screen dark:text-primary text-primary"
+              className="text-3xl md:tracking-[-4px] text-end uppercase md:text-5xl font-black md:mix-blend-screen dark:text-primary text-primary"
             >
               {user?.properties.tags.multi_select[3].name}
             </motion.h1>
-            <motion.span animate={{ opacity: [0, 1] }} transition={{ duration: 1, ease: ease, delay: 2 }} className="text-primary dark:text-primary mix-blend-screen">
+            <motion.span animate={{ opacity: [0, 1] }} transition={{ duration: 1, ease: ease, delay: 2 }} className="text-primary dark:text-primary md:mix-blend-screen">
               Based in {user?.properties.province.rich_text[0].text.content}, {user?.properties.country.rich_text[0].text.content}
             </motion.span>
           </div>
