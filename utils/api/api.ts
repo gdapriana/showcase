5fe630abf2e43e892c100889a9eba09dedacf869
gdapriana <gedeapriana36@gmail.com>
2026-02-19 01:46:18 +0800

    certificates page

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -2,5 +2,5 @@ import axios, {AxiosInstance} from "axios";
 
 
 export const notionApi: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_SERVER,
 })
