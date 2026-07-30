200d077d3a235d54f8d14d564e08acb3e617b1dc
gdapriana <gedeapriana36@gmail.com>
2026-07-30 10:28:05 +0800

    2 columns for projects page

--- app/(root)/projects/_components/wrapper.tsx
+++ app/(root)/projects/_components/wrapper.tsx
@@ -8,7 +8,7 @@ export default function Wrapper({ items }: { items?: NotionPage[] | null }) {
   return (
     <div className="w-full flex-col px-4 relative flex justify-start items-center">
       <div className="max-w-[800px] py-28 relative w-full flex flex-col gap-8 justify-center items-center">
-        <div className="columns-2 md:columns-3 gap-x-2 gap-y-2 ">{items && items.map((project: NotionPage, index: number) => <ProjectCard index={index} project={project} key={project.id} />)}</div>
+        <div className="columns-2 md:columns-2 gap-x-2 gap-y-2 ">{items && items.map((project: NotionPage, index: number) => <ProjectCard index={index} project={project} key={project.id} />)}</div>
       </div>
       <div className="p-8 z-20">
         <Button asChild>
